f571aa29115ab6c9ad53434605c718de67484370
Aditya Singh <singhaditya6774@gmail.com>
2022-11-21 17:19:35 +0530

    Create find value of variable after performing operations.cpp

---
+++ String/find value of variable after performing operations.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int ans=0;
+        for(int i=0;i<operations.size();i++)
+        {
+            if(operations[i]=="X++" || operations[i] =="++X") ans+=1;
+            if(operations[i]=="X--" || operations[i] =="--X") ans-=1;
+        }
+        return ans;
+    }
+};
