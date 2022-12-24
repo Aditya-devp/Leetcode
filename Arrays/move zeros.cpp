a44b00916fa9229c86f3b7cf6981a35ffd61775f
Aditya Singh <singhaditya6774@gmail.com>
2022-12-24 21:16:50 +0530

    Create move zeros.cpp

---
+++ Arrays/move zeros.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        vector<int>v;
+        int count =0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=0){
+                 swap(nums[i],nums[count]);
+                 count++;
+            }
+       
+        }
+        
+    }
+};
