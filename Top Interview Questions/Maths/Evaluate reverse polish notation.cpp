eab872e2c4a5bf18ba381614ea8995a68414af48
Aditya Singh <singhaditya6774@gmail.com>
2023-03-31 10:37:14 +0530

    Create Evaluate reverse polish notation.cpp

---
+++ Top Interview Questions/Maths/Evaluate reverse polish notation.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int>s;
+        for(string x:tokens){
+            if(x=="+"){
+                int a=s.top();
+                s.pop();
+                int b=s.top();
+                s.pop();
+                s.push(a+b);
+            }
+           else if(x=="-"){
+                int a=s.top();
+                s.pop();
+                int b=s.top();
+                s.pop();
+                s.push(b-a);
+            }
+          else  if(x=="*"){
+                int a=s.top();
+                s.pop();
+                int b=s.top();
+                s.pop();
+                s.push(a*b);
+            }
+           else if(x=="/"){
+                int a=s.top();
+                s.pop();
+                int b=s.top();
+                s.pop();
+                s.push(b/a);
+            }
+            else{
+                s.push(stoi(x));
+            }
+        }
+        return s.top();
+    }
+};
