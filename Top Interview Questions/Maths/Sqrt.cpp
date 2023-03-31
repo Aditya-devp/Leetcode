8acc22cdc8e7cbb82931624ba3331abcce445f70
Aditya Singh <singhaditya6774@gmail.com>
2023-03-31 11:01:00 +0530

    Create Sqrt.cpp

---
+++ Top Interview Questions/Maths/Sqrt.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        long long s=0,e=INT_MAX,ans=0;
+        while(s<=e){
+           long long mid=s+(e-s)/2;
+            if(mid*mid<=x){
+                ans=mid;
+                s=mid+1;
+            }
+            else{
+                e=mid-1;
+            }
+        }
+        return ans;
+
+    }
+};
