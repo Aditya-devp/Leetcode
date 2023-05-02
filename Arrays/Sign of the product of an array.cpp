92d0181780ea426a56ae115347b317d35ba271f3
Aditya Singh <singhaditya6774@gmail.com>
2023-05-02 22:19:30 +0530

    Create Sign of the product of an array.cpp

---
+++ Arrays/Sign of the product of an array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+      
+int count =0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]<0){
+                count++;
+            }
+          if(nums[i]==0){
+              return 0;
+          }
+            
+        }
+        if(count%2==0){
+            return 1;
+        }
+        else {
+            return -1;
+        }
+        return 0;
+    }
+};
